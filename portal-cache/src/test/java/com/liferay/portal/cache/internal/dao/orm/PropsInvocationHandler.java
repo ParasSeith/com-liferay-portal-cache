shuyangzhou/liferay-portal

LPS-75689 Add test coverage.

modules/apps/foundation/portal-cache/portal-cache/src/test/java/com/liferay/portal/cache/internal/dao/orm/PropsInvocationHandler.java
@@ -39,6 +39,12 @@ public class PropsInvocationHandler implements InvocationHandler {
 
 				return "true";
 			}
+
+			if (PropsKeys.VALUE_OBJECT_FINDER_CACHE_MAPPING_THRESHOLD.equals(
+					key)) {
+
+				return "-1";
+			}
 		}
 
 		return null;

ParasSeith/com-liferay-portal-cache

portal-cache/src/test/java/com/liferay/portal/cache/internal/dao/orm/PropsInvocationHandler.java
@@ -39,6 +39,12 @@ public class PropsInvocationHandler implements InvocationHandler {
 
 				return "true";
 			}
+
+			if (PropsKeys.VALUE_OBJECT_FINDER_CACHE_LIST_THRESHOLD.equals(
+					key)) {
+
+				return "-1";
+			}
 		}
 
 		return null;